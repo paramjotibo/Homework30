1a4ae669fcdc0ef9ddcb38a2c72e4320da2492c3
param singh <param759@gmail.com>
2022-04-30 18:08:05 -0400

    first commit

---
+++ April30work/src/main/java/workhome/April30work/App.java
@@ -0,0 +1,13 @@
+package workhome.April30work;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ April30work/src/test/java/workhome/April30work/FirstTest.java
@@ -0,0 +1,13 @@
+package workhome.April30work;
+
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+	@Test
+	public void goal()
+
+{
+		System.out.println("home test");
+		}
+}
